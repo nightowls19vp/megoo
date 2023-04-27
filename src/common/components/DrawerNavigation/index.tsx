4971c2cf013b6c379d700ac448ee0eb8022f31a0
Chau Ho <hongocminhchau1911@gmail.com>
2023-04-27 21:05:30 +0700

    Feat: Add logout confirm modal + custom drawer navigation

--- src/common/components/DrawerNavigation/index.tsx
+++ src/common/components/DrawerNavigation/index.tsx
@@ -5,16 +5,49 @@ import {
   DrawerItemList,
 } from '@react-navigation/drawer';
 // import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-
+import Icon from 'react-native-vector-icons/AntDesign';
 import RouteNames from '../../../constants/route-names.const';
 import BottomNavigationBar from '../BottomNavigationBar';
+import {Text, View, Image} from 'react-native';
+import userStore from '../../store/user.store';
+import {Colors} from '../../../constants/color.const';
 
 const Drawer = createDrawerNavigator();
 
 function CustomDrawerContent(props: any) {
   return (
-    <DrawerContentScrollView {...props}>
-      <DrawerItemList {...props} />
+    <DrawerContentScrollView
+      {...props}
+      contentContainerStyle={{
+        flex: 1,
+        paddingTop: 0,
+      }}>
+      <View
+        style={{
+          // backgroundColor: Colors.primary,
+          height: '30%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Image
+          source={{
+            uri: userStore.avatar,
+          }}
+          style={{
+            width: 150,
+            height: 150,
+            borderRadius: 150 / 2,
+            marginBottom: 20,
+          }}
+        />
+        <Text style={{color: Colors.primary, fontSize: 20, fontWeight: 'bold'}}>
+          {userStore.name}
+        </Text>
+      </View>
+      <View style={{flex: 1}}>
+        <DrawerItemList {...props} />
+      </View>
     </DrawerContentScrollView>
   );
 }
@@ -23,11 +56,16 @@ export default function DrawerNavigation() {
   return (
     <Drawer.Navigator
       initialRouteName={RouteNames.HOME_TAB}
-      drawerContent={props => <CustomDrawerContent {...props} />}>
+      drawerContent={props => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        drawerLabelStyle: {marginLeft: -15},
+        drawerActiveTintColor: Colors.primary,
+      }}>
       <Drawer.Screen
         options={{
           title: 'Megoo',
           drawerLabel: 'Trang chủ',
+          drawerIcon: ({color}) => <Icon name="home" size={20} color={color} />,
         }}
         name={RouteNames.HOME_TAB}
         component={BottomNavigationBar}
@@ -37,6 +75,9 @@ export default function DrawerNavigation() {
         options={{
           title: 'Megoo',
           drawerLabel: 'Quản lý gói',
+          drawerIcon: ({color}) => (
+            <Icon name="addusergroup" size={20} color={color} />
+          ),
         }}
         name={RouteNames.PACKAGE}
         component={BottomNavigationBar}
@@ -46,6 +87,7 @@ export default function DrawerNavigation() {
         options={{
           title: 'Megoo',
           drawerLabel: 'Quản lý kho',
+          drawerIcon: ({color}) => <Icon name="isv" size={20} color={color} />,
         }}
         name={RouteNames.STORAGE}
         component={BottomNavigationBar}
@@ -55,6 +97,7 @@ export default function DrawerNavigation() {
         options={{
           title: 'Megoo',
           drawerLabel: 'Trang cá nhân',
+          drawerIcon: ({color}) => <Icon name="user" size={20} color={color} />,
         }}
         name={RouteNames.PROFILE_STACK}
         component={BottomNavigationBar}
@@ -64,6 +107,9 @@ export default function DrawerNavigation() {
         options={{
           title: 'Megoo',
           drawerLabel: 'Cài đặt',
+          drawerIcon: ({color}) => (
+            <Icon name="setting" size={20} color={color} />
+          ),
         }}
         name={RouteNames.SETTINGS_STACK}
         component={BottomNavigationBar}
--- src/screens/settings/screens/SettingsScreen/index.tsx
+++ src/screens/settings/screens/SettingsScreen/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {Button, Text, TouchableOpacity, View} from 'react-native';
 import {Colors} from '../../../../constants/color.const';
 import RouteNames from '../../../../constants/route-names.const';
 import {logout} from './services/settings.service';
@@ -7,8 +7,11 @@ import styles from './styles/styles';
 import userStore from './../../../../common/store/user.store';
 import {ILogoutRes} from './interfaces/logout.interface';
 import Toast from 'react-native-toast-message';
+import Modal from 'react-native-modal';
 
 export default function SettingsScreen({navigation}: {navigation: any}) {
+  const [modalVisible, setModalVisible] = React.useState(false);
+
   return (
     <View style={styles.container}>
       <View style={styles.settingsContainer}>
@@ -32,24 +35,77 @@ export default function SettingsScreen({navigation}: {navigation: any}) {
       <TouchableOpacity
         style={styles.button}
         onPress={() => {
-          logout(userStore.refreshToken).then((response: ILogoutRes) => {
-            if (response.statusCode === 200) {
-              Toast.show({
-                type: 'success',
-                text1: 'Đăng xuất thành công',
-                autoHide: true,
-                visibilityTime: 1000,
-                topOffset: 20,
-                bottomOffset: 20,
-                onHide: () => {
-                  navigation.navigate(RouteNames.LOGIN as never, {} as never);
-                },
-              });
-            }
-          });
+          setModalVisible(!modalVisible);
+          // logout(userStore.refreshToken).then((response: ILogoutRes) => {
+          //   if (response.statusCode === 200) {
+          //     Toast.show({
+          //       type: 'success',
+          //       text1: 'Đăng xuất thành công',
+          //       autoHide: true,
+          //       visibilityTime: 1000,
+          //       topOffset: 20,
+          //       bottomOffset: 20,
+          //       onHide: () => {
+          //         navigation.navigate(RouteNames.LOGIN as never, {} as never);
+          //       },
+          //     });
+          //   }
+          // });
         }}>
         <Text style={styles.buttonText}>Đăng xuất</Text>
       </TouchableOpacity>
+
+      <Modal isVisible={modalVisible}>
+        <View
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'white',
+            borderRadius: 10,
+            gap: 20,
+            padding: 20,
+          }}>
+          <Text style={{fontSize: 24, textAlign: 'justify'}}>
+            Đăng xuất khỏi tài khoản của bạn?
+          </Text>
+
+          <View
+            style={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'flex-end',
+              alignItems: 'center',
+              flexDirection: 'row',
+              gap: 30,
+            }}>
+            <TouchableOpacity
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+              style={{
+                alignItems: 'center',
+              }}>
+              <Text style={{fontSize: 18}}>Huỷ</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                setModalVisible(!modalVisible);
+                logout(userStore.refreshToken).then((response: ILogoutRes) => {
+                  console.log(response.message);
+                  userStore.setAccessToken('');
+                  userStore.setRefreshToken('');
+                  navigation.navigate(RouteNames.LOGIN as never, {} as never);
+                });
+              }}
+              style={{
+                alignItems: 'center',
+              }}>
+              <Text style={{fontSize: 18, color: 'red'}}>Đăng xuất</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
 
       <Toast position="top"></Toast>
     </View>
