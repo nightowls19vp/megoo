bbecd5709b72f8c172663fbe741e912a14f214bb
Chau Ho <hongocminhchau1911@gmail.com>
2023-05-01 00:26:11 +0700

    Add google sign in service

--- App.tsx
+++ App.tsx
@@ -16,6 +16,8 @@ import LoginScreen from './src/screens/login/screens/LoginScreen';
 import RegisterScreen from './src/screens/register/RegisterScreen';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {ActivityIndicator, View} from 'react-native';
+import {URL_HOST} from './src/core/config/api/api.config';
+import axios from 'axios';
 
 const Stack = createNativeStackNavigator();
 
@@ -29,6 +31,18 @@ const App = () => {
 
     if (accessToken !== null) {
       setIsLoggedIn(true);
+      // const validateEndpoint = 'api/auth/validate';
+      // const reqUrl = `${URL_HOST}${validateEndpoint}`;
+      // console.log(reqUrl);
+      // const res = await axios.get(reqUrl, {
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //     Accept: 'application/json',
+      //     Authorization: `Bearer ${accessToken}`,
+      //   },
+      // });
+      // console.log(res.data);
+      // return res.data;
     }
     const refreshToken = await AsyncStorage.getItem('refreshToken');
   };
