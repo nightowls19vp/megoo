dc9f1f6b69e8f6f08cb8b30cb19287a2a818f48f
Ngô Minh Phát <nmphat01062001@gmail.com>
2023-08-01 14:04:50 +0700

    update: current ngrok server

--- src/core/config/api/api.config.ts
+++ src/core/config/api/api.config.ts
@@ -1,3 +1,3 @@
 // export const URL_HOST = "https://megoo.103-97-124-110.flashvps.xyz/be/";
-export const URL_HOST = 'http://localhost:3000/';
-export const URL_SOCKET = 'http://localhost:3001/';
+export const URL_HOST = 'https://74a4-113-173-4-218.ngrok-free.app/';
+export const URL_SOCKET = 'https://ab29-113-173-4-218.ngrok-free.app/';
