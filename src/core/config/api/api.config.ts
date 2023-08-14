ea7c21e8ae9ecc54a9e5448cfd865f7d8c908c6b
Phat Ngo <nmphat01062001@gmail.com>
2023-08-14 12:18:09 +0700

    fix: revert to localhost

--- src/core/config/api/api.config.ts
+++ src/core/config/api/api.config.ts
@@ -2,5 +2,5 @@
 // export const URL_SOCKET = 'https://megoo.103-97-124-110.flashvps.xyz/socket/';
 
 // localhost
-export const URL_HOST = 'https://6479-14-186-187-100.ngrok-free.app/';
-export const URL_SOCKET = 'https://0b24-14-186-187-100.ngrok-free.app/';
+export const URL_HOST = 'http://localhost:3000/';
+export const URL_SOCKET = 'http://localhost:3001/';
